5a58085bd06ba4dc2f6a4bad144b8c65aa329779
raduceaca1234 <ceaca.radu@yahoo.com>
2020-04-20 01:06:21 +0300

    added a textField and another function

--- DemoApp/app/src/main/java/com/robpercival/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/robpercival/demoapp/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view){
 
-        Log.i("Info","Button Pressed!");
+        EditText myTextField = (EditText)findViewById(R.id.myTextField);
+
+        Log.i("Info",myTextField.getText().toString());
 
 
     }
