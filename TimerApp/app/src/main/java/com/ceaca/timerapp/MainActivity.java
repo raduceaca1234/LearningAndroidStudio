d0ba582efe97b9fe2c87117a45daf5dabedbefce
raduceaca1234 <ceaca.radu@yahoo.com>
2020-05-03 03:58:04 +0300

    finish the app

--- TimerApp/app/src/main/java/com/ceaca/timerapp/MainActivity.java
+++ TimerApp/app/src/main/java/com/ceaca/timerapp/MainActivity.java
@@ -1,34 +1,78 @@
 package com.ceaca.timerapp;
 
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    Button controllerButton;
+    Boolean counterActive = false;
+    CountDownTimer countDownTimer;
+
+    public void resetTimer(){
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        countDownTimer.cancel();
+        controllerButton.setText("GO!");
+        timerSeekBar.setEnabled(true);
+        counterActive=false;
+    }
+    public void updateTimer(int secondsLeft){
+        int minutes = (int) secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+        String secondsString = Integer.toString(seconds);
+        if(seconds <= 9){
+            secondsString = "0" + secondsString;
+        }
+        //Log.i("second string",secondsString.toString());
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondsString);
+
+    }
+    public void controlTimer(View view) {
+        if (!counterActive) {
+            counterActive = true;
+            timerSeekBar.setEnabled(false);
+            controllerButton.setText("Stop");
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+                }
+
+                @Override
+                public void onFinish() {
+                    resetTimer();
+                    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mplayer.start();
+                }
+            }.start();
+        } else {
+            resetTimer();;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final SeekBar timerSeekBar = (SeekBar)findViewById(R.id.timerSeekBar);
-        final TextView timerTextView = (TextView)findViewById(R.id.textView);
+        timerSeekBar = (SeekBar)findViewById(R.id.timerSeekBar);
+        timerTextView = (TextView)findViewById(R.id.textView);
+        controllerButton = (Button)findViewById(R.id.controllerButton);
         timerSeekBar.setMax(600);
         timerSeekBar.setProgress(30);
         timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int minutes = (int) progress / 60;
-                int seconds = progress - minutes * 60;
-                String secondsString = Integer.toString(seconds);
-                if(secondsString.equals("0")){
-                    Log.i("second string 0",secondsString.toString());
-                    secondsString = "00";
-                }
-                Log.i("second string",secondsString.toString());
-                timerTextView.setText(Integer.toString(minutes) + ":" + secondsString);
-
+                updateTimer(progress);
             }
 
 
